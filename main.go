168e3c0a3b4cf1263cb90f76360723e0732c263b
sovietninja <sovietninjaru@gmail.com>
2026-07-16 15:23:11 +0000

    2

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
